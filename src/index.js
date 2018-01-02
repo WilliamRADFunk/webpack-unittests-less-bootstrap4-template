16faa835f603079c87effcf991c0b71065f48ff2
William RAD Funk <contact@WILLIAMRADFunk.com>
2018-01-02 01:21:29 -0500

    Uses a little of bootstrap 4 flexbox to make sure it works.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import 'stylesheets/styles.scss';
 import 'stylesheets/styles.less';
 import cat from 'images/cat.gif';
+import cat2 from 'images/cat2.gif';
+import cat3 from 'images/cat3.gif';
 
 class testClass {
 	testFunc(param) {
-		document.write(`here is a tester function to test ES6 to ES5 compilation: ${param}`);
-		document.getElementById('demo-image').src = cat;
+		document.getElementById('injected-h3').innerHTML = `here is a tester function to test ES6 to ES5 compilation: ${param}`;
+		document.getElementById('demo-image1').src = cat;
+		document.getElementById('demo-image2').src = cat2;
+		document.getElementById('demo-image3').src = cat3;
 	}
 }
 
